c61e03a83fef4cfe958a1a6c505e2858857ff4db
Florian Weiss <florian.weiss@weissheiten.at>
2016-01-15 16:39:59 +0100

    initial commit

---
+++ Classes/Weissheiten/OAuth2/ClientInstagram/Provider/InstagramProvider.php
@@ -0,0 +1,26 @@
+<?php
+namespace Weissheiten\OAuth2\ClientInstagram\Provider;
+
+/*                                                                                      *
+ * This script belongs to the TYPO3 Flow package "Weissheiten.OAuth2.ClientInstagram".  *
+ *                                                                                      *
+ * It is free software; you can redistribute it and/or modify it under                  *
+ * the terms of the GNU General Public License, either version 3 of the                 *
+ * License, or (at your option) any later version.                                      *
+ *                                                                                      *
+ * The TYPO3 project - inspiring people to share!                                       *
+ *                                                                                      */
+
+use Flowpack\OAuth2\Client\Token\AbstractClientToken;
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Log\SecurityLoggerInterface;
+use TYPO3\Flow\Security\Account;
+use TYPO3\Flow\Security\Authentication\TokenInterface;
+use TYPO3\Flow\Security\Exception\UnsupportedAuthenticationTokenException;
+
+/**
+ */
+class FacebookProvider extends AbstractClientProvider {
+
+
+}
